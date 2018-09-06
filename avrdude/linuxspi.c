4e9d3352d7f395b773fae420bf2a59e4ca160b1c
Ralf Ramsauer <ralf@vmexit.de>
2018-09-06 23:45:16 +0200

    linuxspi: review linuxspi_chip_erase
    Signed-off-by: Ralf Ramsauer <ralf@vmexit.de>

--- avrdude/linuxspi.c
+++ avrdude/linuxspi.c
@@ -288,19 +288,16 @@ static int linuxspi_program_enable(PROGRAMMER *pgm, AVRPART *p)
     return 0;
 }
 
-static int linuxspi_chip_erase(PROGRAMMER* pgm, AVRPART* p)
+static int linuxspi_chip_erase(PROGRAMMER *pgm, AVRPART *p)
 {
-    unsigned char cmd[4];
-    unsigned char res[4];
+    unsigned char cmd[4], res[4];
 
-    if (p->op[AVR_OP_CHIP_ERASE] == NULL)
-    {
+    if (!p->op[AVR_OP_CHIP_ERASE]) {
         fprintf(stderr, "%s: error: chip erase instruction not defined for part \"%s\"\n", progname, p->desc);
         return -1;
     }
 
     memset(cmd, 0, sizeof(cmd));
-
     avr_set_bits(p->op[AVR_OP_CHIP_ERASE], cmd);
     pgm->cmd(pgm, cmd, res);
     usleep(p->chip_erase_delay);
